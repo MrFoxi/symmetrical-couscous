fc89bdb482946d02709fcd9d7b20f2c63134d6de
MrFoxi <clementperdrix@orange.fr>
2025-04-02 11:00:38 +0200

    Feat : Ajout des calculs Matricielles, de plus de question et du format de réponses

--- quiz.go
+++ quiz.go
@@ -3,53 +3,71 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
+	"strconv"
+	"strings"
 )
 
 type Exercise struct {
-	Question string
-	Answer   float64
+	Question  string
+	Answer    float64
+	AnswerStr string
 }
 
-func GenerateExercise(level int) Exercise {
-	rand.Seed(time.Now().UnixNano())
-
-	switch level {
+func GenerateExercise(step int) Exercise {
+	switch step {
 	case 1:
-		return generateEasy()
+		return generateBasicAddSub(0, 10)
 	case 2:
-		return generateMedium()
+		return generateBasicAddSub(20, 100)
 	case 3:
-		return generateHard()
+		return generateMultDiv()
+	case 4:
+		return generateEquation()
+	case 5:
+		return generateMatrix()
 	default:
-		return generateEasy()
+		return generateBasicAddSub(0, 10)
 	}
 }
 
-func generateEasy() Exercise {
-	a := rand.Intn(10)
-	b := rand.Intn(10)
-	op := rand.Intn(3)
+func generateBasicAddSub(min int, max int) Exercise {
+	a := rand.Intn(max-min) + min
+	b := rand.Intn(max-min) + min
+	op := rand.Intn(2)
 	var question string
 	var answer float64
 
-	switch op {
-	case 0:
+	if op == 0 {
 		question = fmt.Sprintf("Combien font %d + %d ?", a, b)
 		answer = float64(a + b)
-	case 1:
+	} else {
 		question = fmt.Sprintf("Combien font %d - %d ?", a, b)
 		answer = float64(a - b)
-	case 2:
-		question = fmt.Sprintf("Combien font %d * %d ?", a, b)
-		answer = float64(a * b)
 	}
 
 	return Exercise{Question: question, Answer: answer}
 }
 
-func generateMedium() Exercise {
-	a := rand.Intn(10) + 1 // éviter 0
+func generateMultDiv() Exercise {
+	a := rand.Intn(12) + 1
+	b := rand.Intn(12) + 1
+	op := rand.Intn(2)
+	var question string
+	var answer float64
+
+	if op == 0 {
+		question = fmt.Sprintf("Combien font %d * %d ?", a, b)
+		answer = float64(a * b)
+	} else {
+		question = fmt.Sprintf("Combien font %d / %d ?", a*b, a)
+		answer = float64(b)
+	}
+
+	return Exercise{Question: question, Answer: answer}
+}
+
+func generateEquation() Exercise {
+	a := rand.Intn(9) + 1
 	b := rand.Intn(10)
 	x := rand.Intn(10)
 	c := a*x + b
@@ -60,17 +78,30 @@ func generateMedium() Exercise {
 	return Exercise{Question: question, Answer: answer}
 }
 
-func generateHard() Exercise {
-	a := rand.Intn(5) + 1
-	b := rand.Intn(3) + 2
+func generateMatrix() Exercise {
+	a := [2][2]int{{rand.Intn(10), rand.Intn(10)}, {rand.Intn(10), rand.Intn(10)}}
+	b := [2][2]int{{rand.Intn(10), rand.Intn(10)}, {rand.Intn(10), rand.Intn(10)}}
+	var result [2][2]int
 
-	// Exemple : dérivée de ax^b
-	question := fmt.Sprintf("Quelle est la dérivée de f(x) = %dx^%d ?", a, b)
-	answer := float64(a * b) // ax^b → abx^(b-1), on attend juste le coefficient ici
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			result[i][j] = a[i][j] + b[i][j]
+		}
+	}
 
-	return Exercise{Question: question, Answer: answer}
+	formattedAnswer := fmt.Sprintf("[[%d %d] [%d %d]]", result[0][0], result[0][1], result[1][0], result[1][1])
+	question := fmt.Sprintf("Additionne ces matrices 2x2 :\nA = %v\nB = %v\nRéponds sous le format : [[a b] [c d]]", a, b)
+
+	return Exercise{Question: question, AnswerStr: formattedAnswer}
 }
 
-func CheckAnswer(userAnswer float64, correctAnswer float64) bool {
-	return userAnswer == correctAnswer
+func CheckAnswer(userInput string, ex Exercise, isMatrix bool) bool {
+	if isMatrix {
+		return strings.TrimSpace(userInput) == strings.TrimSpace(ex.AnswerStr)
+	}
+	userAnswer, err := strconv.ParseFloat(strings.TrimSpace(userInput), 64)
+	if err != nil {
+		return false
+	}
+	return userAnswer == ex.Answer
 }
