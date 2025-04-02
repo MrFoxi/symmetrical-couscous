4c0bc56768ff90284027d1b200769cbb68f4090a
MrFoxi <clementperdrix@orange.fr>
2025-04-02 11:00:38 +0200

    Feat : Ajout des calculs Matricielles, de plus de question et du format de réponses

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -12,42 +11,55 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	score := 0
-	total := 5 // nombre de questions
+	total := 25 // nombre total de questions
 
 	fmt.Println("Bienvenue dans MathQuizzer ! 🔢")
 	fmt.Println("------------------------------")
-
-	fmt.Print("Choisis ton niveau (1 = Facile, 2 = Moyen, 3 = Difficile) : ")
-	levelInput, _ := reader.ReadString('\n')
-	levelInput = strings.TrimSpace(levelInput)
-	level, err := strconv.Atoi(levelInput)
-	if err != nil || level < 1 || level > 3 {
-		fmt.Println("Niveau invalide, niveau facile sélectionné par défaut.")
-		level = 1
-	}
+	fmt.Println("Tu vas passer par 5 étapes de plus en plus dures :")
+	fmt.Println("1 à 5     → Additions/Soustractions simples")
+	fmt.Println("6 à 10    → Additions/Soustractions complexes")
+	fmt.Println("11 à 15   → Multiplications / Divisions")
+	fmt.Println("16 à 20   → Équations")
+	fmt.Println("21 à 25   → Matrices 🧠")
+	fmt.Println()
 
 	for i := 1; i <= total; i++ {
-		ex := GenerateExercise(level)
-		fmt.Printf("Question %d/%d: %s\n", i, total, ex.Question)
+		var step int
 
+		switch {
+		case i >= 1 && i <= 5:
+			step = 1
+		case i >= 6 && i <= 10:
+			step = 2
+		case i >= 11 && i <= 15:
+			step = 3
+		case i >= 16 && i <= 20:
+			step = 4
+		case i >= 21 && i <= 25:
+			step = 5
+		}
+
+		fmt.Printf("Étape %d - Question %d/%d", step, i, total)
+
+		ex := GenerateExercise(step)
+
+		fmt.Println(ex.Question)
 		start := time.Now()
 
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
 
-		userAnswer, err := strconv.ParseFloat(input, 64)
-		if err != nil {
-			fmt.Println("⛔ Entrée invalide. Réponse considérée comme fausse.")
-			continue
-		}
+		isMatrix := step == 5
 
-		duration := time.Since(start).Seconds()
-
-		if CheckAnswer(userAnswer, ex.Answer) {
-			fmt.Printf("✅ Correct ! (%.2fs)\n", duration)
+		if CheckAnswer(input, ex, isMatrix) {
+			fmt.Printf("✅ Correct ! (%.2fs)\n", time.Since(start).Seconds())
 			score++
 		} else {
-			fmt.Printf("❌ Mauvais ! La bonne réponse était %.0f\n", ex.Answer)
+			if isMatrix {
+				fmt.Printf("❌ Mauvais ! La bonne réponse était : %s\n", ex.AnswerStr)
+			} else {
+				fmt.Printf("❌ Mauvais ! La bonne réponse était %.0f\n", ex.Answer)
+			}
 		}
 		fmt.Println()
 	}
